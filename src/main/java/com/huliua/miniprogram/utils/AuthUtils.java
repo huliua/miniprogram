8077c42f08e6f24019f04b795c53cd3930f1ad3c
huliua <1148247580@qq.com>
2023-03-14 21:57:24 +0800

    完善JWT鉴权的功能 注意：在登录的之后，需要把用户的auth信息存储到token中

--- src/main/java/com/huliua/miniprogram/utils/AuthUtils.java
+++ src/main/java/com/huliua/miniprogram/utils/AuthUtils.java
@@ -1,38 +1,31 @@
 package com.huliua.miniprogram.utils;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTCreator;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.Claim;
-import com.huliua.miniprogram.annotation.CheckAuth;
-import org.springframework.util.StringUtils;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.huliua.miniprogram.annotation.CheckAuth;
+import com.huliua.miniprogram.entity.User;
 
 public class AuthUtils {
 
-    private static final String SING = "huliua";
+    // 签名过期时间
+    public static final long EXPIRE_TIME = 60L * 60 * 1000 * 25 * 365;
+    // 随机生成密钥
+    private static final String SECRET = UUID.randomUUID().toString();
 
     /***
      * 校验token合法性
+     * 
      * @param request
      */
-    public static boolean checkToken(HttpServletRequest request) {
-        if (null == request) {
-            return false;
-        }
-        String token = request.getHeader("token");
-        return StringUtils.hasLength(token);
-    }
-
-    /***
-     * 校验token合法性
-     * @param request
-     */
-    public static boolean checkToken(CheckAuth checkAuth, HttpServletRequest request) {
+    public static boolean checkAuth(CheckAuth checkAuth, HttpServletRequest request) {
         // 不用登录，则放行
         if (!checkAuth.needLogin()) {
             return true;
@@ -40,72 +33,77 @@ public class AuthUtils {
         if (null == request) {
             return false;
         }
-        String token = request.getHeader("token");
-        // 检验token有效性
-        return validateToken(checkAuth.auth(), token);
-    }
 
-    /**
-     * 校验token的有效性
-     */
-    public static boolean validateToken(String[] auth, String token) {
-        // 校验token的合法性
-        if (!validateToken(token)) {
+        // 检验token有效性
+        String token = request.getHeader("token");
+        if (!verifyToken(token)) {
             return false;
         }
-        // 校验token的权限
-        return !validateAuthByToken(auth, token);
+
+        // 校验auth
+        return validateAuthByToken(checkAuth.auth(), token);
     }
 
-    /**
-     * 校验token的有效性
-     */
-    public static boolean validateToken(String token) {
-        // 请求头中没有token则禁止访问
-        return StringUtils.hasLength(token);
-    }
-
-    public static boolean validateAuthByToken(String[] authArr, String token) {
+    private static boolean validateAuthByToken(String[] authArr, String token) {
         if (null == authArr || authArr.length == 0) {
             return true;
         }
-        String auth = getAuthByToken(token);
-        return true;
-    }
-
-    public static String getAuthByToken(String token) {
-        Map<String, Claim> tokenInfo = getTokenInfo(token);
-        return tokenInfo.get("auth").asString();
+        User user = getUserByToken(token);
+        return Arrays.asList(authArr).indexOf(user.getAuth()) >= 0;
     }
 
     /**
      * 生成token
      *
-     * @param map
      * @return
      */
-    public static String genToken(HashMap<String, String> map) {
-        //获取日历对象
-        Calendar calendar = Calendar.getInstance();
-        //默认1小时过期
-        calendar.add(Calendar.HOUR, 1);
-        //新建一个JWT的Builder对象
-        JWTCreator.Builder builder = JWT.create();
-        //将map集合中的数据设置进payload
-        map.forEach((k, v) -> {
-            builder.withClaim(k, v);
-        });
-        //设置过期时间和签名
-        return builder.withExpiresAt(calendar.getTime()).sign(Algorithm.HMAC256(SING));
+    public static String genToken(User user) {
+        Date expireDate = new Date(System.currentTimeMillis() + EXPIRE_TIME);
+        return JWT.create().withClaim("userId", user.getUserId()).withClaim("username", user.getUsername())
+            .withClaim("wid", user.getWid()).withClaim("auth", user.getAuth()).withExpiresAt(expireDate)
+            .sign(Algorithm.HMAC256(SECRET));
     }
 
     /**
-     * 验签并返回DecodedJWT
+     * 获取token内的携带的用户信息
      *
-     * @param token 令牌
-     * @return
+     * @param token token
+     * @return 用户信息
      */
-    public static Map<String, Claim> getTokenInfo(String token) {
-        return JWT.require(Algorithm.HMAC256(SING)).build().verify(token).getClaims();
+    public static User getUserByToken(String token) {
+        DecodedJWT decodedJWT = JWT.decode(token);
+        User user = new User();
+        user.setUserId(decodedJWT.getClaim("userId").asString());
+        user.setUsername(decodedJWT.getClaim("username").asString());
+        user.setWid(decodedJWT.getClaim("wid").asString());
+        return user;
+    }
+
+    /**
+     * 校验token是否合法
+     *
+     * @param token
+     */
+    public static boolean verifyToken(String token) {
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET)).build();
+            verifier.verify(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /***
+     * 校验token合法性
+     * 
+     * @param request
+     */
+    public static boolean verifyToken(HttpServletRequest request) {
+        if (null == request) {
+            return false;
+        }
+        String token = request.getHeader("token");
+        return verifyToken(token);
     }
 }
